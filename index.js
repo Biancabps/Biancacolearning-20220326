f7725e18104c22a7f85e6fb22464f6aab968f924
Bianca <bpsbianca@gmail.com>
2022-03-26 12:21:17 -0300

    feat: usando IF/ELSE

--- index.js
+++ index.js
@@ -43,3 +43,10 @@ if(filme.indicadoParaMaiores || filme.avaliacao > 5) {
 if(!filme.indicadoParaMaiores && (filme.avaliacao > 5 || filme.ano === 1990)) {
     console.log(`[If 07] O filme ${filme.titulo} é indicado para todas as idades e está acima da média.`)
 }
+
+// Condicional com IF/ELSE
+if(filme.indicadoParaMaiores) {
+    console.log(`[IF 08 IF] O filme ${filme.titulo} NÃO é indicado para todas as idades`)
+} else {
+    console.log(`[IF 08 ELSE] O filme ${filme.titulo} é indicado para todas as idades`)
+}
